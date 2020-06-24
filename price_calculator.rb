acfbfa6833f7842b221a3a130fe203bd4cf8df4d
Giju Ambrose <giju@techversantinfo.com>
2020-06-24 17:32:37 +0530

    Naming changes and rspec for check promotions

--- price_calculator.rb
+++ price_calculator.rb
@@ -1,24 +1,22 @@
 # frozen_string_literal: true
-
 require 'pry'
-
-# class for calculating the price of items.
+# Class for calculating the price of items.
 class PriceCalculator
   attr_reader :items
 
   def initialize(items)
     @items = items
 
-    process_calculation
+    calculate
   end
 
-  def process_calculation
-    item_quantity = check_quantity(items)
+  def calculate
+    item_quantity = group_items_by_quantity(items)
 
-    item_price = item_quantity.each_with_object({}) do |item, acc|
-      type = item.first.to_sym
+    item_price = item_quantity.each_with_object({}) do |item, cart|
+      product = item.first.to_sym
       quantity = item.last
-      acc[type] = calculate_price(type, quantity).merge(quantity: quantity)
+      cart[product] = calculate_price(product, quantity).merge(quantity: quantity)
     end
 
     {
@@ -29,55 +27,71 @@ class PriceCalculator
 
   def calculate_total_amounts(item_price)
     item_price.each_with_object(Hash.new(0.0)) do |item, final|
-      final[:discounted_cost] += item[:discounted_cost]
-      final[:actual_cost] += item[:actual_cost]
+      final[:discounted_price] += item[:discounted_price]
+      final[:actual_price] += item[:actual_price]
     end
   end
 
-  def check_quantity(items)
+  def group_items_by_quantity(items)
     items.group_by(&:itself).transform_values(&:count)
   end
 
-  def price_items
+  def promotions
     {
-      A: { unit_price: 30, sale: { price: 75, n: 3 } },
-      B: { unit_price: 20, sale: { price: 35, n: 2 } },
+      A: { unit_price: 30, discount: { qty: 3, new_price: 75} },
+      B: { unit_price: 20, discount: { qty: 2, new_price: 35 } },
       C: { unit_price: 50 },
       D: { unit_price: 15 }
     }
   end
 
   def calculate_price(item, quantity)
-    price = price_items[item]
-    cost = { actual_cost: price[:unit_price] * quantity }
 
-    return cost.merge(discounted_cost: cost[:actual_cost]) unless price[:sale] && price[:sale][:n] == quantity
+    price = promotions[item]
 
-    cost[:discounted_cost] = discounted_price(item, quantity)
+    cost = { actual_price: price[:unit_price] * quantity }
+
+    return cost.merge(discounted_price: cost[:actual_price]) unless price[:discount] && price[:discount][:qty] <= quantity
+
+    cost[:discounted_price] = discounted_price(item, quantity)
     cost
   end
 
   def discounted_price(item, quantity)
-    price = price_items[item]
-    unit_quantity = quantity % price[:sale][:n]
-
-    price[:unit_price] * unit_quantity + price[:sale][:price]
+    price = promotions[item]
+    if quantity % price[:discount][:qty] != 0
+      unit_quantity = quantity % price[:discount][:qty]
+      price[:unit_price] * unit_quantity + price[:discount][:new_price]
+    elsif quantity % price[:discount][:qty] == 0
+      unit_quantity = quantity / price[:discount][:qty]
+      price[:discount][:new_price] * unit_quantity
+    end 
   end
 end
 
+# Executes the programs from here
+
+puts 'items          Price'
+puts '-------------------'
+
+puts "A\t\t $30"
+puts "B\t\t $20"
+puts "C\t\t $50"
+puts "D\t\t $15"
+
 puts 'Please enter all the items purchased separated by a comma:'
-items = gets.chomp.split(',').collect(&:strip)
-item_price = PriceCalculator.new(items).process_calculation
+items = gets.chomp.upcase.split(',').collect(&:strip)
+item_price = PriceCalculator.new(items).calculate
 puts "\n"
-puts 'Item          Quantity          Price'
+puts 'Items         Quantity          Price'
 puts '-------------------------------------'
 item_price[:items].each do |item, _value|
   items = item_price[:items][item]
-  puts "#{item}\t\t #{items[:quantity]}\t\t $#{items[:discounted_cost]}"
+  puts "#{item}\t\t #{items[:quantity]}\t\t $#{items[:discounted_price]}"
   puts "\n"
 end
-item_actual_total = item_price[:total][:actual_cost]
-item_discount_total = item_price[:total][:discounted_cost]
+item_actual_total = item_price[:total][:actual_price]
+item_discount_total = item_price[:total][:discounted_price]
 discount_for_basket_value = item_discount_total > 150 ? 20 : 0
 puts "Total price: $#{item_discount_total - discount_for_basket_value}"
 puts "You saved $#{(item_actual_total - (item_discount_total - discount_for_basket_value)).round(2)} today."
--- spec/price_calculator_spec.rb
+++ spec/price_calculator_spec.rb
@@ -5,31 +5,54 @@ require 'pry'
 describe "price_calculator" do
 
   context "price calculation with valid input" do
-
-
-    it "should return total" do
-      items_price = PriceCalculator.new(["A", "B"]).process_calculation
-      expect(items_price.keys.to_s).to include("total")
-      expect(items_price[:total].keys.to_s).to include('discounted_cost')
-      expect(items_price[:total].keys.to_s).to include('actual_cost')
-      expect(items_price[:total][:discounted_cost]).to eq 50.0
-      expect(items_price[:total][:actual_cost]).to eq 50.0
-    end
+    let!(:multiple_Aes) { PriceCalculator.new(["A", "A", "B"]) }
+    let!(:multiple_Bes) { PriceCalculator.new(["A", "B", "B"]) }
 
     it "should return quantity of each item" do
-      check_quantity = PriceCalculator.new(["A", "B"]).check_quantity(["A", "B"])
-      expect(check_quantity["A"]).to eq 1
-      expect(check_quantity["B"]).to eq 1
+      item_quantity = multiple_Aes.group_items_by_quantity(["A", "A", "B"])
+      expect(item_quantity["A"]).to eq 2
+      expect(item_quantity["B"]).to eq 1
+    end
+
+
+    it "should calculate the discounts for A correctly" do
+      quantity = multiple_Aes.group_items_by_quantity(["A", "A", "B"])
+      promotions = multiple_Aes.promotions
+      if promotions[:A][:qty] == quantity["A"]
+        expect(promotions[:A][:new_price]).to eq 75
+      else
+        expect(promotions[:A][:new_price]).to eq nil
+      end
+    end
+
+    it "should calculate the discounts for B correctly" do 
+      quantity = multiple_Bes.group_items_by_quantity(["A", "A", "B"])
+      promotions = multiple_Aes.promotions
+      if promotions[:B][:qty] == quantity["B"]
+        expect(promotions[:B][:new_price]).to eq 35
+      else
+        expect(promotions[:B][:new_price]).to eq nil
+      end
     end
 
     it "should return unit price with respect to quantity" do
-      check_price = PriceCalculator.new(["A", "B"]).price_items
+      check_price = multiple_Aes.promotions
       expect(check_price[:A].keys.to_s).to include('unit_price')
-      expect(check_price[:A].keys.to_s).to include('sale')
+      expect(check_price[:A].keys.to_s).to include('discount')
       expect(check_price[:B].keys.to_s).to include('unit_price')
-      expect(check_price[:B].keys.to_s).to include('sale')
+      expect(check_price[:B].keys.to_s).to include('discount')
       expect(check_price[:C].keys.to_s).to include('unit_price')
       expect(check_price[:D].keys.to_s).to include('unit_price')
     end
+
+    it "should return the correct total" do
+      items_price = multiple_Aes.calculate
+      expect(items_price.keys.to_s).to include("total")
+      expect(items_price[:total].keys.to_s).to include('discounted_price')
+      expect(items_price[:total].keys.to_s).to include('actual_price')
+      expect(items_price[:total][:discounted_price]).to eq 80.0
+      expect(items_price[:total][:actual_price]).to eq 80.0
+    end
+
   end
 end
